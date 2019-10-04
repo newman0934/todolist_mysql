4ff2d2bfc04188ec201bf1c9693a726d12c8bf32
caesar <newman0934@gmail.com>
2019-10-04 17:28:03 +0800

    add login using passport and express-session

--- app.js
+++ app.js
@@ -13,9 +13,35 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+const session = require('express-session')
+const passport = require('passport')
+
+app.use(session({
+  secret: 'your secret key',                
+  resave: 'false',
+  saveUninitialized: 'false',
+}))
+// 使用 Passport - 要在「使用路由器」前面
+app.use(passport.initialize())
+app.use(passport.session())
+require('./config/passport')(passport)
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  next()
+})
+
+
+
 const db = require("./models")
 const Todo = db.Todo
 const User = db.User
+
+
+//首頁
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
 
 app.use('/users', require('./routes/user'))   
 
--- routes/user.js
+++ routes/user.js
@@ -10,9 +10,12 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 // 登入檢查
-router.post("/login", (req, res, next) => {
-  res.send("登入檢查");
-});
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', {      
+      successRedirect: '/',               
+      failureRedirect: '/users/login',
+    })(req, res, next)
+  })
 // 註冊頁面
 router.get("/register", (req, res) => {
   res.render("register");
