bd3eeba872bce3b5cb31ec8c7fa407db76e0ca1e
caesar <newman0934@gmail.com>
2019-10-04 17:17:36 +0800

    add home and user routers

--- app.js
+++ app.js
@@ -13,36 +13,11 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+const db = require("./models")
+const Todo = db.Todo
+const User = db.User
 
-//首頁
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-
-//登入頁
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-
-// 登入檢查
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
-
-// 註冊頁面
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-
-// 註冊檢查
-app.post('/users/register', (req, res) => {
-  res.send('register')
-})
-
-// 登出
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
+app.use('/users', require('./routes/user'))   
 
 
 // 設定 express port 3000
