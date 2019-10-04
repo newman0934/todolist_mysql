108cb04fb4dcda4c0bd1ef3fd24e0d4ac5ab1bad
caesar <newman0934@gmail.com>
2019-10-04 17:30:44 +0800

    add todo view templates,auth middleware and todo and home routers

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = require('express')               
+const express = require('express')             
 const app = express()                    
-             
+const port = 3000
+const db = require('./models')
+
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
-const port = 3000
+const session = require('express-session')
+const passport = require('passport')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -13,40 +16,29 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-const session = require('express-session')
-const passport = require('passport')
-
 app.use(session({
-  secret: 'your secret key',                
+  secret: 'your secret key',              // secret: 定義一組自己的私鑰（字串)
   resave: 'false',
   saveUninitialized: 'false',
 }))
+
 // 使用 Passport - 要在「使用路由器」前面
 app.use(passport.initialize())
 app.use(passport.session())
+
 require('./config/passport')(passport)
+
 app.use((req, res, next) => {
   res.locals.user = req.user
   next()
 })
 
+// 使用路由器
+app.use('/', require('./routes/home'))
+app.use('/users', require('./routes/user'))  
+app.use('/todos', require('./routes/todo'))    
 
-
-const db = require("./models")
-const Todo = db.Todo
-const User = db.User
-
-
-//首頁
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-
-
-app.use('/users', require('./routes/user'))   
-
-
-// 設定 express port 3000
+// 設定 express port 3000 與資料庫同步
 app.listen(port, () => {
   console.log(`App is running on port ${port}!`)
 })
