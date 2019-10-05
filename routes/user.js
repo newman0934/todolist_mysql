52cf10a776ecee7efd701b28540a3409763f76b1
java5575 <newman0934@gmail.com>
2019-10-05 22:19:01 +0800

    add bcrypt feature

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const bcrypt = require("bcryptjs")
 
 // 載入 user model
 const db = require("../models");
@@ -11,19 +12,28 @@ router.get("/login", (req, res) => {
 });
 // 登入檢查
 router.post('/login', (req, res, next) => {
-    passport.authenticate('local', {      
-      successRedirect: '/',               
-      failureRedirect: '/users/login',
-    })(req, res, next)
-  })
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login',
+  })(req, res, next)
+})
 // 註冊頁面
 router.get("/register", (req, res) => {
   res.render("register");
 });
 // 註冊檢查
 router.post("/register", (req, res) => {
-  const { name, email, password, password2 } = req.body;
-  User.findOne({ where: { email: email } }).then(user => {
+  const {
+    name,
+    email,
+    password,
+    password2
+  } = req.body;
+  User.findOne({
+    where: {
+      email: email
+    }
+  }).then(user => {
     if (user) {
       console.log("User already exists");
       res.render("register", {
@@ -34,22 +44,26 @@ router.post("/register", (req, res) => {
       });
     } else {
       const newUser = new User({
-        //  如果 email 不存在就直接新增
         name,
         email,
         password
       });
-      newUser
-        .save()
-        .then(user => {
-          res.redirect("/"); // 新增完成導回首頁
+      bcrypt.genSalt(10, (err, salt)=>{
+        bcrypt.hash(newUser.password,salt, (err,hash)=>{
+          if(err) throw err
+          newUser.password = hash
+
+          newUser.save().then(user => {
+            res.redirect("/")
+          }).catch(err => console.log(err))
         })
-        .catch(err => console.log(err));
+      })
     }
   });
 });
 // 登出
 router.get("/logout", (req, res) => {
-  res.send("logout");
+  req.logout()
+  res.redirect("/users/login")
 });
 module.exports = router;
